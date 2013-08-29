30572a1bcffed22f2c1ba041d81115ef02cdcbbf
saomi23 <saomichan@gmail.com>
2013-08-30 00:21:18 +0900

    jsのコメントアウトを削除

--- battle_list/js/hover.js
+++ battle_list/js/hover.js
@@ -30,57 +30,5 @@
 		cacheDOMs();
 		setEvents();
 	}, false);
-
-	/*
-	document.addEventListener("DOMContentLoaded", function(e) {
-
-		document.getElementById('back').addEventListener("touchstart", function(e) {
-			var element = document.getElementById('h_back');
-			element.className = "back_img_on";
-		});
-		document.getElementById('back').addEventListener("touchmove", function(e) {
-			console.log("touchmove is called!");
-			var element = document.getElementById('h_back');
-			element.className = "back_img";
-		});
-		document.getElementById('back').addEventListener("touchend", function(e) {
-			var element = document.getElementById('h_back');
-			//element.className = "back_img";
-			if (element.className === "back_img_on") {
-				location.href = "../battle_build/index.html"
-			}
-		});
-
-	*/
-		/* 対戦作成ボタン */
-	/*
-		document.getElementById('a_build').addEventListener("touchstart", function(e) {
-			var element = document.getElementById('h_build');
-			element.className = "build_img_on";
-		})
-		document.getElementById('a_build').addEventListener("touchend", function(e) {
-			var element = document.getElementById('h_build');
-			element.className = "build_img";
-			location.href = "../battle_build/index.html"
-		})
-*/
-
-		/* ハゲドウボタン */
-		/*
-		document.getElementsByClassName('hagedou').addEventListener("touchstart", function(e) {
-			var element = document.getElementsByClassName('hagedou');
-			console.log(element);
-			element.className = "hagedou_on";
-		})
-		*/
-/*
-		document.getElementById('a_back').addEventListener("touchend", function(e) {
-			var element = document.getElementById('h_back');
-			console.log(element);
-			element.className = "back_img";
-			location.href = "../vote/index.html"
-		})
-*/
-
-	//})
+	
 })();
