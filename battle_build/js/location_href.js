640568caf42905cb84877bdce48caf7472835feb
saomi23 <saomichan@gmail.com>
2013-09-05 23:14:16 +0900

    なにしたんだっけ

--- battle_build/js/location_href.js
+++ battle_build/js/location_href.js
@@ -1,22 +1,3 @@
-/*
-(function() {
-	document.addEventListener("DOMContentLoaded", function(e) {
-		
-		// 作成ボタン
-		
-		var builtbtnEl = document.getElementById('builtbtn');
-		builtbtnEl.addEventListener('mousedown',function(){
-			builtbtnEl.className = "builtbtn_img_on";
-		})
-		builtbtnEl.addEventListener('mouseup',function(){
-			builtbtnEl.className = "builtbtn_img";
-			location.href = "../battle_list/index.html"
-		})
-	})
-	
-})();
-*/
-
 (function() {
 
 	//作成ボタン
