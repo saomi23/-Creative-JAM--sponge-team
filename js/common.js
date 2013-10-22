1fc2df71eab77cdcabed1621ba03b6094725983c
saomi23 <bechico@Macintosh.local>
2013-10-22 15:18:20 +0900

    common.jsのコメントアウト削除

--- js/common.js
+++ js/common.js
@@ -3,20 +3,6 @@
 	//backBtn
 	var backBtnEl      = null;
 	var backBtnIconEl  = null;
-/*
-	
-	//footer1
-	var footer1     = null;
-	var footer1Bg   = null;
-	
-	//footer2
-	var footer2     = null;
-	var footer2Bg   = null;
-	
-	//footer3
-	var footer3     = null;
-	var footer3Bg   = null;
-*/
 
 	function cacheDOMs () {
 		//backBtn
@@ -27,19 +13,6 @@
 		buildBtnEl     = document.getElementById('build');
 		buildBtnIconEl = document.getElementById('h_build');
 		
-/*
-		//footer1
-		footer1        = document.getElementById('ftli01');
-		footer1Bg      = document.getElementById('ft01');
-		
-		//footer2
-		footer2        = document.getElementById('ftli02');
-		footer2Bg      = document.getElementById('ft02');
-		
-		//footer3
-		footer3        = document.getElementById('ftli03');
-		footer3Bg      = document.getElementById('ft03');
-*/
 	};
 
 	function setEvents () {
@@ -55,47 +28,7 @@
 				history.back();
 			}
 		});
-/*
-		
-		//footer1
-		footer1.addEventListener("touchstart", function(e) {
-			footer1Bg.className = "ftli01_bg_on";
-		});
-		footer1.addEventListener("touchmove", function(e) {
-			footer1Bg.className = "ftli01_bg";
-		});
-		footer1.addEventListener("touchend", function(e) {
-			if (footer1Bg.className === "ftli01_bg_on") {
-				location.href = "../battle_list/index.html"
-			}
-		});
-		
-		//footer2
-		footer2.addEventListener("touchstart", function(e) {
-			footer2Bg.className = "ftli02_bg_on";
-		});
-		footer2.addEventListener("touchmove", function(e) {
-			footer2Bg.className = "ftli02_bg";
-		});
-		footer2.addEventListener("touchend", function(e) {
-			if (footer2Bg.className === "ftli02_bg_on") {
-				location.href = "../mypage/index.html"
-			}
-		});
-		
-		//footer3
-		footer3.addEventListener("touchstart", function(e) {
-			footer3Bg.className = "ftli03_bg_on";
-		});
-		footer3.addEventListener("touchmove", function(e) {
-			footer3Bg.className = "ftli03_bg";
-		});
-		footer3.addEventListener("touchend", function(e) {
-			if (footer3Bg.className === "ftli03_bg_on") {
-				location.href = "../battle_build/index.html"
-			}
-		});
-*/
+
 	}
 
 	document.addEventListener("DOMContentLoaded", function (e) {
